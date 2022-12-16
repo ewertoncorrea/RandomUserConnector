a145f14cc21be516b6827d66c3235827530f90f3
Ewerton <ewerton.leao@gmail.com>
2022-12-16 21:16:44 +0100

    removed logs and unused imports

--- src/main/java/com/ewerton/randomuserconnector/schedulers/ConnectorScheduler.java
+++ src/main/java/com/ewerton/randomuserconnector/schedulers/ConnectorScheduler.java
@@ -1,11 +1,8 @@
 package com.ewerton.randomuserconnector.schedulers;
 
-import com.ewerton.randomuserconnector.controllers.RandomUserAPIController;
 import com.ewerton.randomuserconnector.models.RandomUser;
 import com.ewerton.randomuserconnector.models.Root;
 import com.ewerton.randomuserconnector.repository.UserRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -13,12 +10,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 public class ConnectorScheduler {
-
-    private static final Logger log = LoggerFactory.getLogger(ConnectorScheduler.class);
 
     @Value("${url}")
     private String url;
@@ -45,7 +39,6 @@ public class ConnectorScheduler {
                                         result.getLocation().getCountry(), result.getGender()))
                         .findFirst().orElse(null);
                 users.add(user);
-                log.info("loaded user" + user.toString());
                 repository.saveAll(users);
             }
         }
